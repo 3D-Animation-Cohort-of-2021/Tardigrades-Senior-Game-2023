d0ad05a6f4d03e802a873b153c48776b721f7af9
William Lemmons <will.e.lemmons@gmail.com>
2023-02-27 19:59:44 -0700

    Lets the CenterPointBehavior function without target group in hierarchy

--- Assets/Scripts/CenterPointBehaviour.cs
+++ Assets/Scripts/CenterPointBehaviour.cs
@@ -14,17 +14,18 @@ public class CenterPointBehaviour : MonoBehaviour {
     private CinemachineTargetGroup cTgroup;
     public GameObject targetGroup;
     void Start() {
-        //ref to PlayerTargetGroup (Will)
-        cTgroup = targetGroup.GetComponent<CinemachineTargetGroup>();
-        
-        
+
         for (int i = 0; i < amountToSpawn; i++) {
             var newPoint = RandomPointInRadius();
             var newPiglet = Instantiate(pigletPrefab, newPoint, quaternion.identity);
             newPiglet.GetComponent<FollowPointBehaviour>().pointObject = 
                 Instantiate(pointObject, newPoint, quaternion.identity, transform);
-            //Send new instantited to target group list (Will)
-            cTgroup.AddMember(newPiglet.transform,1f,5f );
+            //ref to PlayerTargetGroup and Send new instantited to target group list (Will)
+            if (targetGroup != null)
+            {
+                cTgroup = targetGroup.GetComponent<CinemachineTargetGroup>();
+                cTgroup.AddMember(newPiglet.transform, 1f, 5f);
+            }
         }
     }
 
