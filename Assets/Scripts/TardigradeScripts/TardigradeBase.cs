140c4a3c3ec368c73cd39b0ca6411e94fed6e189
Marshall Krueger <mgkrueger4@gmail.com>
2023-09-18 12:29:55 -0400

    Adjusting heal functionality

--- Assets/Scripts/TardigradeScripts/TardigradeBase.cs
+++ Assets/Scripts/TardigradeScripts/TardigradeBase.cs
@@ -24,7 +24,7 @@ public abstract class TardigradeBase : MonoBehaviour, IDamageable
     private Animator[] _animators;
 
     protected FollowPointBehaviour _followBehavior;
-    private VisualEffect _healEffect;
+    protected VisualEffect _healEffect;
 
     protected float _damage = 1;
 
