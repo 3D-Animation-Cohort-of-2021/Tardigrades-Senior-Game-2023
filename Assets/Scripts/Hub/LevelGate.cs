8616472cdcf47eff022cc1161e292406c994005d
Marshall Krueger <mgkrueger4@gmail.com>
2024-01-19 15:03:46 -0500

    Set up level data manager

--- Assets/Scripts/Hub/LevelGate.cs
+++ Assets/Scripts/Hub/LevelGate.cs
@@ -8,12 +8,38 @@ public class LevelGate : MonoBehaviour
     [SerializeField]
     private string _sceneName;
 
+    [SerializeField]
+    private LevelData _levelData;
+
+    private void Start()
+    {
+        
+        if(_levelData != null && _levelData.levelUnlocked)
+        {
+            SetupGate();
+        }
+    }
+
+    private void SetupGate()
+    {
+        Collider collider = GetComponent<Collider>();
+
+        if (_levelData.levelUnlocked)
+        {
+            collider.isTrigger = true;
+        }
+        else
+        {
+            collider.isTrigger = false;
+        }
+    }
+
 
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
         
-        if(other.tag == "Player" && other.gameObject.layer == LayerMask.NameToLayer("Center"))
+        if(other.tag == "Player" && other.gameObject.layer == LayerMask.NameToLayer("Center") && _levelData.levelUnlocked)
         {
            LoadScene();
         }
