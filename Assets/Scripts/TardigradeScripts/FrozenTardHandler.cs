6074dfc1a4aee0547c459b5858ad5e6087600b31
SwiggettDJ <swiggettdj@gmail.com>
2024-02-25 17:00:37 -0700

    Finished Tard Freeze Death

--- Assets/Scripts/TardigradeScripts/FrozenTardHandler.cs
+++ Assets/Scripts/TardigradeScripts/FrozenTardHandler.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FrozenTardHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool physicsOn;
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.GetComponent<TardigradeBase>() != null && !physicsOn)
+        {
+            EnablePhysics();
+            physicsOn = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void EnablePhysics()
     {
-        
+        Rigidbody[] rigidbodys = GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody body in rigidbodys)
+        {
+            body.useGravity = true;
+        }
+
+        BoxCollider[] colliders = GetComponentsInChildren<BoxCollider>();
+        foreach (BoxCollider collider in colliders)
+        {
+            collider.enabled = true;
+        }
     }
 }
